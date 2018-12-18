65f51fb48301b9a442150f6f91e51499caff507d
Sarah Cohen <cohen.sarah591@gmail.com>
2018-12-18 00:00:05 -0500

    worked on perpetual motion

--- PerpetualMotion/app/src/main/java/com/example/sabrahamson/perpetualmotion/activities/MainActivity.java
+++ PerpetualMotion/app/src/main/java/com/example/sabrahamson/perpetualmotion/activities/MainActivity.java
@@ -1,25 +1,125 @@
 package com.example.sabrahamson.perpetualmotion.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.perpetual_motion.pm_game.IDGame;
 import com.example.sabrahamson.perpetualmotion.R;
+import com.example.sabrahamson.perpetualmotion.lib.CardPilesAdapter;
+
+import java.util.EmptyStackException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private IDGame mCurrentGame;
+
+    private CardPilesAdapter mAdapter;
+
+    // pref boolean fields
+    private boolean mPrefUseAutoSave, mPrefShowErrors;
+
+    // Preference keys
+    private final String mKeyPrefsName = "PREFS";
+    private String mKeyAutoSave, mKeyShowErrors, mKeyGame, mKeyCheckedPiles;
+    private int countOfChecks = 0;
+
+    private void setDefaultValuesForPreferences ()
+    {
+        PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.main_prefs, true);
+    }
+
+    @Override
+    protected void onStop() {
+        saveToSharedPref();
+        super.onStop();
+    }
+
+    private void saveToSharedPref() {
+        SharedPreferences preferences = getSharedPreferences(mKeyPrefsName, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.clear();
+
+        saveSettingsToSharedPrefs (editor);
+        saveGameAndBoardToSharedPrefsIfAutoSaveIsOn (editor);
+
+        editor.apply();
+    }
+
+    private void saveSettingsToSharedPrefs(SharedPreferences.Editor editor) {
+        editor.putBoolean(mKeyShowErrors, mPrefShowErrors);
+        editor.putBoolean(mKeyAutoSave, mPrefUseAutoSave);
+    }
+
+    private void saveGameAndBoardToSharedPrefsIfAutoSaveIsOn(SharedPreferences.Editor editor) {
+        if (mPrefUseAutoSave)
+        {
+            final boolean [] CHECKED_PILES = mAdapter.getCheckedPiles();
+
+            //editor.putString(mKeyGame, getJSONof(mCurrentGame));
+            for (int i = 0; i < CHECKED_PILES.length; i++) {
+                editor.putBoolean(mKeyCheckedPiles + i, CHECKED_PILES[i]);
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setDefaultValuesForPreferences();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+        setupFAB();
+        setupBoard();
+        restoreAppSettingsFromPrefs();
+    }
+
+    private void restoreAppSettingsFromPrefs() {
+        SharedPreferences preferences = getSharedPreferences(mKeyPrefsName, MODE_PRIVATE);
+
+        mPrefUseAutoSave = preferences.getBoolean(mKeyAutoSave, true);
+        mPrefShowErrors = preferences.getBoolean(mKeyShowErrors, true);
+    }
+
+    private void setupBoard() {
+        // create the adapter
+        mAdapter = new CardPilesAdapter(getApplicationContext(), R.string.cards_in_stack);
+
+        //Set the listener we made at the bottom as the Adapter's OIClick Listener Object
+        mAdapter.setOnItemClickListener(listener);
+
+        // get a reference to the RecyclerView
+        RecyclerView rvPiles = findViewById(R.id.rv_piles);
+
+        // set the number of columns based on the currently loaded integers.xml
+        final int RV_COLUMN_COUNT = getResources().getInteger(R.integer.rv_columns);
+
+        // Create the layout manager
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager
+                (this, RV_COLUMN_COUNT);
+
+        rvPiles.setHasFixedSize(true);
+        rvPiles.setLayoutManager(layoutManager);
+        rvPiles.setAdapter(mAdapter);
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void setupFAB() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +128,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
     }
 
     @Override
@@ -39,23 +138,121 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.action_toggle_auto_save).setChecked(mPrefUseAutoSave);
+        menu.findItem(R.id.action_turn_show_error_messages).setChecked(mPrefShowErrors);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // dismiss the snackbar if shown
+        switch (id) {
+            //case R.id.home: --in a secondary activity - this handles the back button
+            //  onBackPressed();
+            case R.id.action_about:
+                showAbout();
+                return true;
+            case R.id.action_toggle_auto_save:
+                toggleMenuItem(item);
+                mPrefUseAutoSave = item.isChecked();
+                return true;
+            case R.id.action_turn_show_error_messages:
+                toggleMenuItem(item);
+                mPrefShowErrors = item.isChecked();
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void dismissSnackBarIfShown() {
+
+    }
+
+
+
+    private void toggleMenuItem(MenuItem item) {
+        item.setChecked(!item.isChecked());
+    }
+
+    private void showAbout() {
+
     }
 
     public void turn_action_discard(View view) {
+        if(mCurrentGame.isGameOver()) {
+            showSB_alreadyGameOver();
+        } else {
+            dismissSnackBarIfShown();
+            final boolean[] checkedPiles = mAdapter.getCheckedPiles();
+            attemptDiscard(checkedPiles, getCountOfChecks(checkedPiles));
+        }
+    }
+
+    private void attemptDiscard(boolean[] checkPiles, int countOfChecks) {
+        try {
+            discardOneOrTwo(checkPiles, countOfChecks);
+            doUpdatesAfterGameStartOrTakingTurn();
+        } catch(EmptyStackException ese) {
+            showSB(getString(R.string.error_cannot_discard_from_empty_pile));
+        } catch(UnsupportedOperationException uoe) {
+            showSB(uoe.getMessage());
+        }
+    }
+
+    private void showSB(String string) {
+
+    }
+
+    private void doUpdatesAfterGameStartOrTakingTurn() {
+
+    }
+
+    private void discardOneOrTwo(boolean[] checkedPiles, int countOfChecks) {
+        int pileTopToDiscard, secondPileTopToDiscard;
+
+        switch (countOfChecks) {
+            case 1:
+                pileTopToDiscard = getCheckedItem(checkedPiles, 0);
+                mCurrentGame.discardOneLowestOfSameSuit(pileTopToDiscard);
+            case 2:
+                pileTopToDiscard = getCheckedItem(checkedPiles, 0);
+                secondPileTopToDiscard = getCheckedItem(checkedPiles, 0);
+
+        }
+    }
+
+    private int getCountOfChecks(boolean[] checkPiles) {
+        return 0;
+
+    }
+
+    private void showSB_alreadyGameOver() {
     }
 
     public void turn_action_deal(View view) {
     }
+
+    private final CardPilesAdapter.OIClickListener listener = new CardPilesAdapter.OIClickListener() {
+        @Override
+        public void onItemClick(int position, View v) {
+            try {
+                if(mCurrentGame.getNumberOfCardsInStackAtPosition(position) > 0) {
+                    if(mCurrentGame.isGameOver()) {
+                        //show Game Over Snackbar
+                    } else {
+                        //dismiss Snackbar if shown
+                        mAdapter.toggleCheck(position);
+                    }
+                }
+            } catch (Exception e){
+                Log.d("STACK_CRASH", e.getMessage());
+            }
+        }
+    };
 }
--- PerpetualMotion/app/src/main/java/com/example/sabrahamson/perpetualmotion/lib/CardPilesAdapter.java
+++ PerpetualMotion/app/src/main/java/com/example/sabrahamson/perpetualmotion/lib/CardPilesAdapter.java
@@ -1,11 +1,14 @@
 package com.example.sabrahamson.perpetualmotion.lib;
 
+import android.animation.Animator;
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -13,70 +16,197 @@ import android.widget.TextView;
 import com.example.perpetual_motion.pm_game.Card;
 import com.example.sabrahamson.perpetualmotion.R;
 
-public class CardPilesAdapter extends RecyclerView.Adapter<CardPilesAdapter.ViewHolder> {
+import java.util.Locale;
 
-    //adapter main data source
+public class CardPilesAdapter extends RecyclerView.Adapter<CardPilesAdapter.ViewHolder>
+{
+    // Listener class to pass click event and data back to Activity
+    private static OIClickListener sOIClickListener;
+
+    // Adapter Primary Data Source
     private final Card[] mPILE_TOPS;
 
-    //which piles are checked
-    private boolean[] mCHECKED_PILES;
+    // Parallel arrays to primary source
 
-    //how many cards are in each pile, including the top card
+    // which piles are checked
+    private final boolean[] mCHECKED_PILES;
+
+    // how many cards are underneath each pile
     private final int[] mNUMBER_OF_CARDS_IN_PILE;
-
-    //string reference from XML
     private final String mMSG_CARDS_IN_STACK;
 
-    public CardPilesAdapter(Context context, int ID_MSG_CARDS_IN_STACK) {
-        final int NUMBER_OF_PILES = 4;
-        this.mPILE_TOPS = new Card[NUMBER_OF_PILES];
-        this.mCHECKED_PILES = new boolean[NUMBER_OF_PILES];
-        this.mNUMBER_OF_CARDS_IN_PILE = new int[NUMBER_OF_PILES];
-        this.mMSG_CARDS_IN_STACK = context.getString(ID_MSG_CARDS_IN_STACK);
+    public void setOnItemClickListener (OIClickListener oiClickListener)
+    {
+        CardPilesAdapter.sOIClickListener = oiClickListener;
+    }
+
+    public CardPilesAdapter (Context context, int msgCardsInStack)
+    {
+        int NUMBER_OF_PILES = 4;
+        mPILE_TOPS = new Card[NUMBER_OF_PILES];
+        mCHECKED_PILES = new boolean[NUMBER_OF_PILES];
+        mNUMBER_OF_CARDS_IN_PILE = new int[NUMBER_OF_PILES];
+        mMSG_CARDS_IN_STACK = context.getString (msgCardsInStack);
+    }
+
+    @NonNull @Override public ViewHolder onCreateViewHolder (@NonNull ViewGroup parent, int viewType)
+    {
+        View itemLayoutView = LayoutInflater.from (parent.getContext ()).inflate (
+                R.layout.rv_item_pile_top_card, parent, false);
+        return new ViewHolder (itemLayoutView);
+    }
+
+    @Override public void onBindViewHolder (@NonNull ViewHolder holder, int position)
+    {
+        updateOuterCard (holder, position);
+        updateInnerCard (holder, position);
+    }
+
+    private void updateOuterCard (ViewHolder holder, int position)
+    {
+        holder.tv_pile_card_cards_in_stack.setText (
+                mMSG_CARDS_IN_STACK.concat (Integer.toString (mNUMBER_OF_CARDS_IN_PILE[position])));
+    }
+
+    private void updateInnerCard (ViewHolder holder, int position)
+    {
+        if (mPILE_TOPS[position] != null) {
+            populateAndShowInnerCard (holder, position);
+        }
+        else {
+            clearAndHideInnerCard (holder);
+        }
+    }
+
+    private void populateAndShowInnerCard (ViewHolder holder, int position)
+    {
+
+        Card currentCard = mPILE_TOPS[position];
+        int currentColor = currentCard.getSuit().getColor();
+
+        holder.tv_pile_card_rank_top.setTextColor (currentColor);
+        holder.tv_pile_card_rank_top.setText (
+                String.format (
+                        Locale.getDefault (), "%d", currentCard.getRank ().getValue ()));
+
+        holder.tv_pile_card_suit_center.setTextColor (currentColor);
+        holder.tv_pile_card_suit_center.setText (
+                Character.toString (currentCard.getSuit ().getCharacter ()));
+
+        holder.tv_pile_card_name_bottom.setTextColor (currentColor);
+        holder.tv_pile_card_name_bottom.setText (
+                currentCard.getRank ().toString ());
+
+        holder.cb_pile_card_checkbox.setChecked (mCHECKED_PILES[position]);
+
+        holder.cv_pile_inner_Card.setVisibility (View.VISIBLE);
+    }
+
+    private void clearAndHideInnerCard (ViewHolder holder)
+    {
+        holder.tv_pile_card_rank_top.setText("");
+        holder.tv_pile_card_suit_center.setText("");
+        holder.tv_pile_card_name_bottom.setText("");
+        holder.cb_pile_card_checkbox.setChecked (false);
+
+        holder.cv_pile_inner_Card.setVisibility (View.INVISIBLE);
+    }
+
+    @Override public int getItemCount ()
+    {
+        return mPILE_TOPS.length;
     }
 
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rv_item_pile_top_card, viewGroup, false);
-        return null;
+    public void updatePile (int pileNumber, Card card, int numberOfCardsInStack)
+    {
+        mPILE_TOPS[pileNumber] = card;
+        mNUMBER_OF_CARDS_IN_PILE[pileNumber] = numberOfCardsInStack;
+
+        notifyItemChanged (pileNumber);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) { //i is the position
-        updateOuterCard(viewHolder, i);
-        updateInnerCard(viewHolder, i);
+    public Card getCardAt (int position)
+    {
+        return mPILE_TOPS[position] == null ? null : mPILE_TOPS[position].copy ();
     }
 
-    private void updateOuterCard(ViewHolder viewHolder, int i) {
+    public void clearCheck (int position)
+    {
+        if (mCHECKED_PILES[position]) {
+            mCHECKED_PILES[position] = false;
+
+            notifyItemChanged (position);
+        }
     }
 
-    private void updateInnerCard(ViewHolder viewHolder, int i) {
+    public void toggleCheck (int position)
+    {
+        mCHECKED_PILES[position] = !mCHECKED_PILES[position];
+        notifyItemChanged (position);
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
+    public void overwriteChecksFrom (boolean[] newChecksSet)
+    {
+        System.arraycopy (newChecksSet, 0, mCHECKED_PILES, 0, mCHECKED_PILES.length);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public boolean[] getCheckedPiles ()
+    {
+        return mCHECKED_PILES.clone ();
+    }
 
-        public final CardView cv_pile_inner_card;
+
+    @Override public void onViewAttachedToWindow (@NonNull ViewHolder holder)
+    {
+        super.onViewAttachedToWindow (holder);
+        animateCard (holder.cv_pile_inner_Card);
+    }
+
+    private void animateCard (View view)
+    {
+        if (Build.VERSION.SDK_INT >= 21) {
+            int centerX = 0, centerY = 0, startRadius = 0;
+            int endRadius = Math.max (view.getWidth (), view.getHeight ());
+            Animator circularRevealAnimationOfCard = ViewAnimationUtils.createCircularReveal
+                    (view, centerX, centerY, startRadius, endRadius);
+            circularRevealAnimationOfCard.start ();
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        public final CardView cv_pile_inner_Card;
         public final TextView tv_pile_card_rank_top, tv_pile_card_name_bottom,
-            tv_pile_card_suit_center, tv_pile_card_cards_in_stack;
-
+                tv_pile_card_suit_center, tv_pile_card_cards_in_stack;
         public final CheckBox cb_pile_card_checkbox;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            cv_pile_inner_card = itemView.findViewById(R.id.pile_card_inner_card);
-            tv_pile_card_rank_top = itemView.findViewById(R.id.pile_card_rank_top);
-            tv_pile_card_name_bottom = itemView.findViewById(R.id.pile_card_suit_center);
-            tv_pile_card_suit_center = itemView.findViewById(R.id.pile_card_suit_center);
-            tv_pile_card_cards_in_stack = itemView.findViewById(R.id.pile_card_in_stack);
-            cb_pile_card_checkbox = itemView.findViewById(R.id.pile_card_checkbox);
+        public ViewHolder (View itemLayoutView)
+        {
+            super (itemLayoutView);
 
+            cv_pile_inner_Card = itemLayoutView.findViewById (R.id.pile_card_inner_card);
+            tv_pile_card_rank_top = itemLayoutView.findViewById (R.id.pile_card_rank_top);
+            tv_pile_card_name_bottom = itemLayoutView.findViewById (R.id.pile_card_name_bottom);
+            tv_pile_card_suit_center = itemLayoutView.findViewById (R.id.pile_card_suit_center);
+            tv_pile_card_cards_in_stack = itemLayoutView.findViewById (R.id.pile_card_in_stack);
+            cb_pile_card_checkbox = itemLayoutView.findViewById (R.id.pile_card_checkbox);
+
+            cb_pile_card_checkbox.setClickable (false);
+
+            itemLayoutView.setOnClickListener (this);
         }
+
+        @Override public void onClick (View view)
+        {
+            sOIClickListener.onItemClick (getAdapterPosition (), view);
+        }
+    }
+
+    // used to send data out of Adapter - implemented in the calling Activity/Fragment
+    @SuppressWarnings ("UnusedParameters")
+    public interface OIClickListener
+    {
+        void onItemClick (int position, View v);
     }
 }
